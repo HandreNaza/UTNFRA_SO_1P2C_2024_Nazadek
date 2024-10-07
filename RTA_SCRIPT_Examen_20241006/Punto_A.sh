9c14688e68f3255dfb7c300913dbd43754d24b5a
HandreNaza <handresnaza@gmail.com>
2024-10-07 05:46:58 +0000

    ADD: punto b

--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3}}
+sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
 
