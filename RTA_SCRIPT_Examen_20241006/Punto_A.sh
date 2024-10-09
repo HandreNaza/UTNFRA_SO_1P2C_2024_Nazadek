0b7da08136299ded36c3a82fd33aa3c79bb10865
HandreNaza <handresnaza@gmail.com>
2024-10-09 04:15:37 +0000

    ADD: Montar discos

--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
 
-sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
+
+
 
--- RTA_SCRIPT_Examen_20241006/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_B.sh
@@ -60,7 +60,25 @@ echo
 echo "Mis Particiones: "
 sudo fdisk -l $DISCO
 echo
+echo "Formateo las Particiones: "
+sudo fdisk -l $DISCO |grep $DISCO |grep -viE 'Disk|exten' |awk '{print "sudo mkfs.ext4 "$1}' |/bin/bash 
+echo
+echo "Montando las Particiones:"
 
+sudo mount ${DISCO}1 /Examenes-UTN/alumno_1/parcial_1
+sudo mount ${DISCO}5 /Examenes-UTN/alumno_2/parcial_1
+sudo mount ${DISCO}6 /Examenes-UTN/alumno_2/parcial_2
+sudo mount ${DISCO}7 /Examenes-UTN/alumno_2/parcial_3
+sudo mount ${DISCO}8 /Examenes-UTN/alumno_3/parcial_1
+sudo mount ${DISCO}9 /Examenes-UTN/alumno_3/parcial_2
+sudo mount ${DISCO}10 /Examenes-UTN/alumno_3/parcial_3
+sudo mount ${DISCO}12 /Examenes-UTN/alumno_1/parcial_2
+sudo mount ${DISCO}13 /Examenes-UTN/alumno_1/parcial_3
+sudo mount ${DISCO}11 /Examenes-UTN/profesores
+
+# Validar el montaje
+echo "Particiones montadas:"
+df -h | grep Examenes-UTN
 
 
 
